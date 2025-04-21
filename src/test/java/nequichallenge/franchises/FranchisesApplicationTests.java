7351023ec5edd04b0e2ec54601c12cad043d8c94
Jose Herrera <jose.herrera@pragma.com.co>
2025-04-21 11:56:07 -0500

    implement h2 to test the context load

--- src/test/java/nequichallenge/franchises/FranchisesApplicationTests.java
+++ src/test/java/nequichallenge/franchises/FranchisesApplicationTests.java
@@ -2,12 +2,15 @@ package nequichallenge.franchises;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FranchisesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// method is empty
 	}
 
 }
